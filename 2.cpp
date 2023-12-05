47debb18d2be1f1d7219dcb580fc10753fd061cf
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-05 11:11:55 +0100

    .
    mismo que codigo que en 1.cpp

---
+++ 2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+class Environment {
+private:
+    std::map<std::string, int> symbolTable;
+
+public:
+    void addSymbol(const std::string& symbol, int value) {
+        symbolTable[symbol] = value;
+    }
+
+
+    int getSymbolValue(const std::string& symbol) {
+        if (symbolTable.find(symbol) != symbolTable.end()) {
+            return symbolTable[symbol];
+        } else {
+            std::cerr << "Error: El símbolo '" << symbol << "' no está definido en el entorno." << std::endl;
+            return 0;
+        }
+    }
+
