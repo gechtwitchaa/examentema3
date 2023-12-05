3b52817d7b0d2887f56cf94f602fc9df362d1a9a
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-05 11:26:55 +0100

    .
    4.cpp

--- 3.cpp
+++ 3.cpp
@@ -27,7 +27,7 @@ int main() {
 
     env.addSymbol("x", 10);
     env.addSymbol("y", 20);
-    
+
     std::unique_ptr<int> valueX = env.lookup("x");
     if (valueX) {
         std::cout << "El valor de 'x' es: " << *valueX << std::endl;
