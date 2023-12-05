855bfebd8d3babfe7f8370f7a002349a092e045f
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-05 11:29:42 +0100

    .

--- 4.cpp
+++ 4.cpp
@@ -8,3 +8,24 @@ class Environment {
 private:
     std::map<std::string, int> symbolTable;
 
+public:
+    void addSymbol(const std::string& symbol, int value) {
+        if (symbolTable.find(symbol) != symbolTable.end()) {
+            if (symbolTable[symbol] != value) {
+                throw std::invalid_argument("Intento de insertar un símbolo ya existente con un valor diferente.");
+            }
+            return;
+        }
+        symbolTable[symbol] = value;
+    }
+
+    std::unique_ptr<int> lookup(const std::string& symbol) {
+        auto it = symbolTable.find(symbol);
+        if (it != symbolTable.end()) {
+            return std::make_unique<int>(it->second);
+        } else {
+            throw std::out_of_range("El símbolo no está definido en el entorno.");
+        }
+    }
+};
+
