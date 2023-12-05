1e2f837a320a1af3c5cc66445332efd0f27cec9d
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-05 11:26:04 +0100

    .
    uso punteros unique_ptr

--- 3.cpp
+++ 3.cpp
@@ -27,5 +27,20 @@ int main() {
 
     env.addSymbol("x", 10);
     env.addSymbol("y", 20);
+    
+    std::unique_ptr<int> valueX = env.lookup("x");
+    if (valueX) {
+        std::cout << "El valor de 'x' es: " << *valueX << std::endl;
+    } else {
+        std::cout << "El símbolo 'x' no está definido en el entorno." << std::endl;
+    }
 
-  
+    std::unique_ptr<int> valueZ = env.lookup("z");
+    if (valueZ) {
+        std::cout << "El valor de 'z' es: " << *valueZ << std::endl;
+    } else {
+        std::cout << "El símbolo 'z' no está definido en el entorno." << std::endl;
+    }
+
+    return 0;
+}
