7c8819d9c3a2db23a7dbcb6e2e6012d4b4ca1c02
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-05 11:50:46 +0100

    .
    funcion para eliminar un simbolo

--- 6.cpp
+++ 6.cpp
@@ -5,3 +5,18 @@
 struct Variant {
 };
 
+class Environment {
+private:
+    std::map<std::string, Variant> symbolTable;
+
+public:
+    void removeSymbol(const std::string& symbol) {
+        auto it = symbolTable.find(symbol);
+        if (it != symbolTable.end()) {
+            symbolTable.erase(it);
+            std::cout << "Símbolo '" << symbol << "' eliminado." << std::endl;
+        } else {
+            std::cout << "No se encontró el símbolo '" << symbol << "'." << std::endl;
+        }
+    }
+
