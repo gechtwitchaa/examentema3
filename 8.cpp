7a15d5fe49325cf3dd9c6be5db84d05a63e67b7d
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-05 12:16:56 +0100

    .
    int y strng

--- 8.cpp
+++ 8.cpp
@@ -9,3 +9,8 @@ struct Variant {
     int intValue;
     std::string stringValue;
 
+
+    Variant(int value) : type(Type::INT), intValue(value) {}
+    Variant(const char* value) : type(Type::STRING), stringValue(value) {}
+
+ 
